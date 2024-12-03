a3a14e4195e9ee3d618066457b23b5bc26e07231
Razkkhan <razkhan.raz742@gmail.com>
2024-12-04 01:29:30 +0600

    Home page added

--- js/login.js
+++ js/login.js
@@ -1,8 +1,21 @@
 // console.log('button clicking file added')
 document.getElementById('button-login').addEventListener('click', function(event){
     event.preventDefault();
-console.log('login button clicked');
+
 
 const phoneNumber = document.getElementById('phone-number').value;
-console.log(phoneNumber)
+const pinNumber = document.getElementById('pin-number').value;
+
+
+console.log(phoneNumber, pinNumber)
+
+if(phoneNumber === '5' && pinNumber === '1234'){
+    console.log('You Are Login')
+    window.location.href ='/home.html';
+}
+else{
+    alert('Wrong Phone Number or Pin');
+}
+
+
 })
