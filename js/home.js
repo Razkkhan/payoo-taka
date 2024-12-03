5431ae25216a8ed65cf2aaefe7fb529817737135
Razkkhan <razkhan.raz742@gmail.com>
2024-12-04 01:49:19 +0600

    add Money Password

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log('home Added')
+// console.log('home Added')
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+    // step-2
+const addMoneyInput = document.getElementById('input-add-money').value;
+console.log(addMoneyInput);
+
+    // console.log('money add button clicked')
+
+const pinNumberInput = document.getElementById('input-pin-number').value;
+console.log(pinNumberInput)
+});
