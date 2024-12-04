3b13b1fcadd77f8631be35660e551ffb7c993d59
Razkkhan <razkhan.raz742@gmail.com>
2024-12-04 19:00:13 +0600

    add money

--- js/home.js
+++ js/home.js
@@ -10,4 +10,26 @@ console.log(addMoneyInput);
 
 const pinNumberInput = document.getElementById('input-pin-number').value;
 console.log(pinNumberInput)
+
+if(pinNumberInput === '1234'){
+console.log('adding money to account')
+
+const balance = document.getElementById('account-balance').innerText;
+console.log(balance)
+
+// const newBalance = balance + addMoneyInput;
+const addMoneyNumber = parseFloat(addMoneyInput);
+const balanceNumber = parseFloat(balance);
+const newBalance = addMoneyNumber +balanceNumber;
+console.log(newBalance)
+
+
+document.getElementById('account-balance').innerText = newBalance;
+
+}
+else{
+    alert('Failed to add money! Please try again')
+}
+
+
 });
